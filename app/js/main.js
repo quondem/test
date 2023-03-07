602aa1015b110ed361e8db9c9e0c8e7f9f0cf4bd
Matvey <shul-m@mail.ru>
2023-03-08 02:59:22 +0300

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let signUp = document.querySelector('.sign-up__submit') // кнопка
 let firstName = document.querySelector('.first input') // имя
 let lastName = document.querySelector('.last input') // фамилия
@@ -83,3 +84,90 @@ signUp.addEventListener('click', function (e) {
     clicked = true
 })
 
+=======
+let signUp = document.querySelector('.sign-up__submit') // кнопка
+let firstName = document.querySelector('.first input') // имя
+let lastName = document.querySelector('.last input') // фамилия
+let nation = document.querySelector('.nation input') // национальность
+let email = document.querySelector('.email input') // email
+let password = document.querySelector('.password input') // пароль
+let confirm = document.querySelector('.confirm input') // проверка пароля
+let inputs = [firstName, lastName, nation, email, password, confirm] // все инпуты
+let info = [firstName, lastName, nation] // инпуты без регулярок
+let content = document.querySelector('.sign-up__sign') // весь контент
+let message = document.querySelector('.message')
+
+let patterns = {
+    email: /\w+@\w+\.\w+$/g,
+    password: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/g
+}
+// (?=.* [0 - 9]) - строка содержит хотя бы одно число;
+// (?=.* [a - z]) - строка содержит хотя бы одну латинскую букву в нижнем регистре;
+// (?=.* [A - Z]) - строка содержит хотя бы одну латинскую букву в верхнем регистре;
+// [0 - 9a - zA - Z]{ 8,} - строка состоит не менее, чем из 8 вышеупомянутых символов.
+
+function checkNull() {
+    for (let input of inputs) {
+        if (input.value == '') {
+            input.classList.add('error')
+            return false
+        }
+    }
+    return true
+} // проверка что инпуты не пусты
+
+email.addEventListener('input', function (e) {
+    if (this.value.match(patterns.email)) {
+        this.classList.remove('error')
+    } else {
+        this.classList.add('error')
+    }
+})
+
+password.addEventListener('input', function (e) {
+    if (this.value.match(patterns.password)) {
+        this.classList.remove('error')
+        this.nextElementSibling.classList.add('disabled')
+    } else {
+        this.classList.add('error')
+        this.nextElementSibling.classList.remove('disabled')
+    }
+})
+
+confirm.addEventListener('input', function (e) {
+    if (this.value == password.value) {
+        this.classList.remove('error')
+    } else {
+        this.classList.add('error')
+    }
+})
+
+for (let input of info) {
+    input.addEventListener('input', function () {
+        if (this.value == '') {
+            this.classList.add('error')
+        } else {
+            this.classList.remove('error')
+        }
+    })
+}
+
+let clicked = false
+signUp.addEventListener('click', function (e) {
+    e.preventDefault()
+    if (!clicked) {
+        if (checkNull() && !document.querySelector('.error')) {
+            content.classList.add('disabled')
+            message.classList.remove('disabled')
+        } else {
+            this.classList.add('animate__shakeX')
+            setTimeout(() => {
+                this.classList.remove('animate__shakeX')
+                clicked = false
+            }, 1500)
+        }
+    }
+    clicked = true
+})
+
+>>>>>>> d44a302 (first commit)
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const { src, dest, watch, parallel, series } = require('gulp')
 
 const sass = require('gulp-sass')(require('sass'))
@@ -90,4 +91,98 @@ exports.watching = watching
 exports.browsersync = browsersync
 
 exports.build = series(cleanDist, build)
+=======
+const { src, dest, watch, parallel, series } = require('gulp')
+
+const sass = require('gulp-sass')(require('sass'))
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify-es').default
+const imagemin = require('gulp-imagemin')
+const browserSync = require('browser-sync').create()
+const autoprefixer = require('gulp-autoprefixer')
+const sourcemaps = require('gulp-sourcemaps')
+const clean = require('gulp-clean')
+const babel = require('gulp-babel')
+const htmlmin = require('gulp-htmlmin')
+
+function styles() {
+    return src('app/sass/style.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(autoprefixer({ overrideBrowserlist: ['last 10 version'] }))
+        .pipe(concat('style.min.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream())
+}
+
+function scripts() {
+    return src([
+        'app/js/*.js',
+        '!app/js/main.min.js'
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(babel({ presets: ['@babel/env'] }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream())
+}
+
+function images() {
+    return src('app/images/**/*')
+        .pipe(imagemin(
+            [
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.mozjpeg({ quality: 75, progressive: true }),
+                imagemin.optipng({ optimizationLevel: 5 }),
+                imagemin.svgo({
+                    plugins: [
+                        { removeViewBox: true },
+                        { cleanupIDs: false }
+                    ]
+                })
+            ]
+        ))
+        .pipe(dest('dist/images'))
+}
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+}
+
+function cleanDist() {
+    return src('dist')
+        .pipe(clean())
+}
+
+function build() {
+    return src([
+        'app/css/style.min.css',
+        'app/fonts/**/*',
+        'app/images/**/*',
+        'app/js/main.min.js',
+        'app/*.html'
+    ], { base: 'app' })
+        .pipe(dest('dist'))
+}
+
+function watching() {
+    watch(['app/sass/**/*.sass'], styles)
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
+    watch(['app/*.html']).on('change', browserSync.reload)
+}
+
+exports.styles = styles
+exports.scripts = scripts
+exports.watching = watching
+exports.browsersync = browsersync
+
+exports.build = series(cleanDist, build)
+>>>>>>> d44a302 (first commit)
 exports.default = parallel(styles, scripts, watching, browsersync)
